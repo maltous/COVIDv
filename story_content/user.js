f7d59ba80f860a6c50b7e32ba5463f60438296fa
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-04-11 16:54:08 +0300

    COVIDv

---
+++ story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6gUfYigKX2u":
+        Script1();
+        break;
+      case "5ZkXgEmw09n":
+        Script2();
+        break;
+      case "6h97l6z3FmX":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  window.print();
+}
+
